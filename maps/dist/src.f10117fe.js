06858333abe467d98b6776a947524297c7e3ae78
vianneychin <47989727+vianneychin@users.noreply.github.com>
2019-09-24 12:35:00 -0700

    Add an implements TypeScript variable to the classes to help me as an engineer find my errors more precisely.

--- maps/dist/src.f10117fe.js
+++ maps/dist/src.f10117fe.js
@@ -85113,12 +85113,17 @@ var User =
 /** @class */
 function () {
   function User() {
+    this.color = 'red';
     this.name = faker_1.default.name.firstName();
     this.location = {
       lat: parseFloat(faker_1.default.address.latitude()),
       lng: parseFloat(faker_1.default.address.longitude())
     };
   }
+
+  User.prototype.markerContent = function () {
+    return "User Name: " + this.name;
+  };
 
   return User;
 }();
@@ -85143,6 +85148,7 @@ var Company =
 /** @class */
 function () {
   function Company() {
+    this.color = 'blue';
     this.companyName = faker_1.default.company.companyName();
     this.catchPhrase = faker_1.default.company.catchPhrase();
     this.location = {
@@ -85150,6 +85156,10 @@ function () {
       lng: parseFloat(faker_1.default.address.longitude())
     };
   }
+
+  Company.prototype.markerContent = function () {
+    return "\n      <div>\n        <h1>\n          Company Name: " + this.companyName + "\n        </h1>\n        <h3>\n          Catchphrase: " + this.catchPhrase + "\n        </h3>\n      </div>\n    ";
+  };
 
   return Company;
 }();
@@ -85176,12 +85186,20 @@ function () {
   }
 
   CustomMap.prototype.addMarker = function (mappable) {
-    new google.maps.Marker({
+    var _this = this;
+
+    var marker = new google.maps.Marker({
       map: this.googleMap,
       position: {
         lat: mappable.location.lat,
         lng: mappable.location.lng
       }
+    });
+    marker.addListener('click', function () {
+      var infoWindow = new google.maps.InfoWindow({
+        content: mappable.markerContent()
+      });
+      infoWindow.open(_this.googleMap, marker);
     });
   };
 
